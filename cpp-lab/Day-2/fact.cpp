c8ffaa1b3bdbbcace7ef63e3673edcd031d4e008
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-12-18 03:14:28 +0530

    cpp day 2

---
+++ cpp-lab/Day-2/fact.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int fact(int n)
+{
+	if(n==1)
+		return 1;
+	return n*fact(n-1);
+}
+
+int main()
+{
+	int n;
+	cout<<"Enter number"<<endl;
+	cin>>n;
+	cout<<"Factorial of "<<n<<": "<<fact(n)<<endl;
+        return 0;
+}
